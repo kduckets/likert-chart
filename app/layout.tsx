0c7a37b22e5e873b034590775622150e48a55aa0
kduckets <kmditroia@gmail.com>
2025-02-14 19:00:14 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'steph data',
+  description: 'Created by kev and robots, with love'
 }
 
 export default function RootLayout({
